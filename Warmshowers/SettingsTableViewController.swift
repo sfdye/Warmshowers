a0653c0df2118860aa0033bc5ff8cf1cbef2aba7
Rajan Fernandez <rajan.fernandez@gmail.com>
2015-11-19 16:02:45 +1300

    Added setttings bundle

---
+++ Warmshowers/SettingsTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  SettingsTableViewController.swift
+//  Warmshowers
+//
+//  Created by Rajan Fernandez on 19/11/15.
+//  Copyright © 2015 Rajan Fernandez. All rights reserved.
+//
+
+import UIKit
+
+// TODO: - add: distance units options, route manager, wifi only, map source options
+
+let SWITCH_CELL_ID = "SwitchCell"
+
+class SettingsTableViewController: UITableViewController {
+    
+    var wifiOnly = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        tableView.registerClass(cellClass: SwitchTableViewCell, forCellReuseIdentifier: SWITCH_CELL_ID)
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let reuseIdentifier = SWITCH_CELL_ID
+        let label = "Wi-Fi Only"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as! SwitchTableViewCell
+        
+        if cell.label != nil {
+            cell.label!.text = label
+        }
+
+
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Warmshowers/SwitchTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SwitchTableViewCell.swift
+//  Warmshowers
+//
+//  Created by Rajan Fernandez on 19/11/15.
+//  Copyright © 2015 Rajan Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    @IBOutlet weak var label: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
