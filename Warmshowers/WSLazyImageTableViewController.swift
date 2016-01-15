0d25c16d8164fb5945df19fd6f087e8a3f46a240
Rajan Fernandez <rajan.fernandez@gmail.com>
2016-01-16 00:53:51 +1300

    Recommit to add missed files

---
+++ Warmshowers/WSLazyImageTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  LazyImageTableViewController.swift
+//  Warmshowers
+//
+//  Created by Rajan Fernandez on 15/01/16.
+//  Copyright © 2016 Rajan Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class WSLazyImageTableViewController: UITableViewController {
+    
+    var lazyImageObjects = [AnyObject]()
+    var imageDownloadsInProgress = [NSIndexPath: WSImageDownloader]()
+    var placeHolderImageName: String?
+    var placeHolderImage: UIImage? {
+        guard let placeHolderImageName = placeHolderImageName else {
+            return nil
+        }
+        return UIImage(named: placeHolderImageName)
+    }
+
+    
+    // MARK: Utiliies
+    
+    func getLazyImageDataSourceForIndexPath(indexPath: NSIndexPath) -> WSLazyImageDataSource? {
+        
+        guard lazyImageObjects.count > indexPath.row else {
+            return nil
+        }
+        
+        return lazyImageObjects[indexPath.row] as? WSLazyImageDataSource
+    }
+    
+    // Sets the images for a table view cell at a given index path as either the image provided by the LazyImage data source or a placeholder
+    //
+    func setLazyImageForCell(cell: AnyObject, atIndexPath indexPath: NSIndexPath) {
+        
+        guard let object = getLazyImageDataSourceForIndexPath(indexPath), var cell = cell as? WSLazyImageTableViewCell else {
+                return
+        }
+        
+        // Show the image or a placholder
+        if let image = object.lazyImage {
+            cell.lazyImage = image
+        } else {
+            // Defer downloading images until scrolling ends
+            if tableView.dragging == false && tableView.decelerating == false {
+                startImageDownload(object, indexPath: indexPath)
+            }
+            cell.lazyImage = placeHolderImage
+        }
+    }
+    
+    // Downloads a objects image size profile image and updates the table view data source
+    //
+    func startImageDownload(object: WSLazyImageDataSource, indexPath: NSIndexPath) {
+        
+        if imageDownloadsInProgress[indexPath] == nil {
+            
+            // Create and start a imageDownloader object
+            let imageDownloader = WSImageDownloader()
+            imageDownloader.object = object
+            imageDownloader.placeHolderImage = placeHolderImage
+            imageDownloader.completionHandler = {
+                
+                // Update the cell with the objects profile image
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // Sometimes this cast failes for the first screen of the tableview, but i don't know why.
+                    if var cell = self.tableView.cellForRowAtIndexPath(indexPath) as? WSLazyImageTableViewCell {
+                        cell.lazyImage = object.lazyImage
+                    }
+                })
+                
+                // Remove the downloader object from the downloads
+                self.imageDownloadsInProgress.removeValueForKey(indexPath)
+            }
+            imageDownloadsInProgress[indexPath] = imageDownloader
+            imageDownloader.startDownload()
+        }
+    }
+    
+    // Tries to download all the profile images for the objects on the screen
+    //
+    func loadImagesForObjectsOnScreen() {
+        
+        guard let visiblePaths = tableView.indexPathsForVisibleRows else {
+            return
+        }
+        
+        for indexPath in visiblePaths {
+            if let object = lazyImageObjects[indexPath.row] as? WSLazyImageDataSource {
+                if object.lazyImage == nil {
+                    startImageDownload(object, indexPath: indexPath)
+                }
+            }
+        }
+    }
+    
+}
